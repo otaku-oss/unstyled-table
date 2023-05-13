5d0dc6743f5222e5336e9d9e0e323b741843acc1
Touha Zohair <touha98@gmail.com>
2023-05-14 01:54:04 +0600

    pre - migration commit

--- packages/unstyled-table/vite.config.ts
+++ packages/unstyled-table/vite.config.ts
@@ -4,29 +4,28 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    plugins: [
-        react(),
-        dts({
-            insertTypesEntry: true,
-        }),
-    ],
-    build: {
-        sourcemap: true,
-        lib: {
-            entry: path.resolve(__dirname, 'src/lib/index.ts'),
-            name: 'MyLib',
-            formats: ['es', 'umd'],
-            fileName: (format) => `bundle.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom', 'styled-components'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                    'styled-components': 'styled',
-                },
-            },
-        },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'unstyled-table',
+      formats: ['es', 'umd', 'cjs'],
+      fileName: (format) => `bundle.${format}.js`,
     },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
