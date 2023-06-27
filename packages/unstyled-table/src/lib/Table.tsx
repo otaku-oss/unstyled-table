f1f38040c460874009c7c419fbb5fadf59afe3c9
Touha Zohair <touha98@gmail.com>
2023-06-27 10:16:17 +0600

    rearrange export order

--- packages/unstyled-table/src/lib/Table.tsx
+++ packages/unstyled-table/src/lib/Table.tsx
@@ -74,9 +74,9 @@ const ReactTable = <TData, TValue = any>({ renders, ...props }: TableProps<TData
     },
     filterFns: { fuzzy: fuzzyFilter },
     manualSorting,
-    pageCount: props.manualPagination ? props.pageCount ?? props.pageCount : undefined,
     manualFiltering,
     manualPagination,
+    pageCount: props.manualPagination ? props.pageCount ?? props.pageCount : undefined,
     onSortingChange: props.setSorting ?? setSorting,
     onColumnFiltersChange: props.setColumnFilters ?? setColumnFilters,
     onGlobalFilterChange: props.setGlobalFilter ?? setGlobalFilter,
@@ -99,87 +99,85 @@ const ReactTable = <TData, TValue = any>({ renders, ...props }: TableProps<TData
 
   return (
     <>
-      <div style={{ overflowX: 'auto', maxWidth: '100%' }}>
-        <renderers.Table renderer={renders?.table} tableInstance={table}>
-          <renderers.Header renderer={renders?.header} headerGroups={table.getHeaderGroups()}>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <renderers.HeaderRow headerGroup={headerGroup} key={headerGroup.id} renderer={renders?.headerRow}>
-                {headerGroup.headers.map((header) => (
-                  <renderers.HeaderCell
-                    renderer={renders?.headerCell}
-                    props={{ colSpan: header.colSpan }}
-                    header={header}
-                    key={header.id}
-                  >
-                    {header.isPlaceholder ? null : (
-                      <>
-                        <div
-                          {...{
-                            style: {
-                              display: header.column.getCanSort() ? 'flex' : 'block',
-                              cursor: header.column.getCanSort() ? 'pointer' : 'default',
-                              alignItems: 'center',
-                            },
+      <renderers.Table renderer={renders?.table} tableInstance={table}>
+        <renderers.Header renderer={renders?.header} headerGroups={table.getHeaderGroups()}>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <renderers.HeaderRow headerGroup={headerGroup} key={headerGroup.id} renderer={renders?.headerRow}>
+              {headerGroup.headers.map((header) => (
+                <renderers.HeaderCell
+                  renderer={renders?.headerCell}
+                  props={{ colSpan: header.colSpan }}
+                  header={header}
+                  key={header.id}
+                >
+                  {header.isPlaceholder ? null : (
+                    <>
+                      <div
+                        {...{
+                          style: {
+                            display: header.column.getCanSort() ? 'flex' : 'block',
+                            cursor: header.column.getCanSort() ? 'pointer' : 'default',
+                            alignItems: 'center',
+                          },
 
-                            onClick: header.column.getToggleSortingHandler(),
-                          }}
-                        >
-                          {flexRender(header.column.columnDef.header, header.getContext()) as ReactNode}
-                          {{
-                            asc: ChevronUp,
-                            desc: ChevronDown,
-                          }[header.column.getIsSorted() as string] ?? null}
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(header.column.columnDef.header, header.getContext()) as ReactNode}
+                        {{
+                          asc: ChevronUp,
+                          desc: ChevronDown,
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                      {header.column.getCanFilter() ? (
+                        <div>
+                          <Filter column={header.column} table={table} inputComponent={renders?.filterInput} />
                         </div>
-                        {header.column.getCanFilter() ? (
-                          <div>
-                            <Filter column={header.column} table={table} inputComponent={renders?.filterInput} />
-                          </div>
-                        ) : null}
-                      </>
-                    )}
-                  </renderers.HeaderCell>
-                ))}
-              </renderers.HeaderRow>
-            ))}
-          </renderers.Header>
-          <renderers.Body renderer={renders?.body} rowModel={table.getRowModel()}>
-            {table.getRowModel().rows.map((row) => {
-              return (
-                <renderers.BodyRow renderer={renders?.bodyRow} row={row} key={row.id}>
-                  {row.getVisibleCells().map((cell) => {
-                    return (
-                      <renderers.BodyCell renderer={renders?.bodyCell} props={{}} cell={cell} key={cell.id}>
-                        {flexRender(cell.column.columnDef.cell, cell.getContext()) as ReactNode}
-                      </renderers.BodyCell>
-                    );
-                  })}
-                </renderers.BodyRow>
-              );
-            })}
-          </renderers.Body>
-          {props.showFooter ? (
-            <renderers.Footer renderer={renders?.footer} footerGroups={table.getFooterGroups()}>
-              {table.getFooterGroups().map((footerGroup) => (
-                <renderers.FooterRow key={footerGroup.id} renderer={renders?.footerRow} footerGroup={footerGroup}>
-                  {footerGroup.headers.map((header) => (
-                    <renderers.FooterCell
-                      props={{ colSpan: header.colSpan }}
-                      key={header.id}
-                      renderer={renders?.footerCell}
-                      header={header}
-                    >
-                      {header.isPlaceholder
-                        ? null
-                        : (flexRender(header.column.columnDef.footer, header.getContext()) as ReactNode)}
-                    </renderers.FooterCell>
-                  ))}
-                </renderers.FooterRow>
+                      ) : null}
+                    </>
+                  )}
+                </renderers.HeaderCell>
               ))}
-            </renderers.Footer>
-          ) : null}
-        </renderers.Table>
-      </div>
-      <div style={{ height: 8 }}></div>
+            </renderers.HeaderRow>
+          ))}
+        </renderers.Header>
+        <renderers.Body renderer={renders?.body} rowModel={table.getRowModel()}>
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <renderers.BodyRow renderer={renders?.bodyRow} row={row} key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <renderers.BodyCell renderer={renders?.bodyCell} props={{}} cell={cell} key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext()) as ReactNode}
+                    </renderers.BodyCell>
+                  );
+                })}
+              </renderers.BodyRow>
+            );
+          })}
+        </renderers.Body>
+        {props.showFooter ? (
+          <renderers.Footer renderer={renders?.footer} footerGroups={table.getFooterGroups()}>
+            {table.getFooterGroups().map((footerGroup) => (
+              <renderers.FooterRow key={footerGroup.id} renderer={renders?.footerRow} footerGroup={footerGroup}>
+                {footerGroup.headers.map((header) => (
+                  <renderers.FooterCell
+                    props={{ colSpan: header.colSpan }}
+                    key={header.id}
+                    renderer={renders?.footerCell}
+                    header={header}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : (flexRender(header.column.columnDef.footer, header.getContext()) as ReactNode)}
+                  </renderers.FooterCell>
+                ))}
+              </renderers.FooterRow>
+            ))}
+          </renderers.Footer>
+        ) : null}
+      </renderers.Table>
+
       {props.hidePaginationBar ? null : renders?.paginationBar ? (
         <renders.paginationBar tableInstance={table} />
       ) : (
