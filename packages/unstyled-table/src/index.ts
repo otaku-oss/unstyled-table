ff897672f3aa98129829f259f4f43ee0f6de3214
Touha Zohair <touha98@gmail.com>
2023-11-12 20:07:47 +0600

    update export

--- packages/unstyled-table/src/index.ts
+++ packages/unstyled-table/src/index.ts
@@ -1,5 +1,4 @@
 export { ReactTable as Table } from './lib/table';
-export { PaginationComponent } from './lib/components/pagination';
 export { createColumnHelper } from '@tanstack/react-table';
 export * from './lib/hooks';
 
